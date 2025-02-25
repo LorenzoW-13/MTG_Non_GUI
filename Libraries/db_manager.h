d54532df5c8204ab5b0b760e566361d2d6d49bc8
LorenzoW-13 <lorenzo.simonetti13gmail.com>
2025-02-25 18:38:19 +0100

    Merged all tested features in main. Created new branch "card" for new features. Added "card data" table while removing the redundant data from the cards table. Added some commented code containing the development ideas (pick up from there)

--- Libraries/db_manager.h
+++ Libraries/db_manager.h
@@ -13,4 +13,6 @@ int make_album(sqlite3* db, std::string name);
 
 int make_cell(sqlite3* db, int album_id);
 
+int make_card(sqlite3* db, std::string card_name, int card_number);
+
 #endif
--- db_manager.cpp
+++ db_manager.cpp
@@ -31,12 +31,20 @@ int tables(sqlite3* db) {
     const char* Cards =     "CREATE TABLE IF NOT EXISTS Cards ("
                             "id INTEGER PRIMARY KEY AUTOINCREMENT, " //Card id, progressive
                             "name TEXT NOT NULL, " //Card name, will match the storage cell's name
-                            "color TEXT, " //Card color identifier as W (white), B (black), U (blue), G (green), R (red) or N (colorless)
                             "c_set TEXT NOT NULL, " //Card's set
-                            "cost TEXT NOT NULL, " //Mana cost of the card, which uses the same symbols as the color
                             "price DECIMAL DEFAULT NULL, " //Price of the card, currently set to null
                             "cell_id INT NOT NULL, " //Cell of storage. NOTE: in future i will be adding a deck storing function
                             "FOREIGN KEY (cell_id) REFERENCES CellCards(id));";
+    const char* CData =     "CREATE TABLE IF NOT EXISTS CData ("
+                            "name TEXT PRIMARY KEY, " //Cards are saved and idicised by their name
+                            "color TEXT, " //Card color identifier as W (white), B (black), U (blue), G (green), R (red) or N (colorless)
+                            "cost TEXT NOT NULL, " //Mana cost of the card, which uses the same symbols as the color
+                            "sutype TEXT DEFAULT NULL, " //Card Supertype, ex: Basic, Legendary, Snow and World
+                            "typing TEXT NOT NULL, " //Card Type (Land, Creature, Artifact, Enchantment, Planeswalker, Battle, Instant, Sorcery) and Subtype
+                            "stats TEXT DEFAULT NULL, " //If creature, vehicle ecc, the strenght and toughness in form S/T (ex: 1/1)
+                            "wording TEXT DEFAULT NULL, " //Text of the card. SET TO DEFAULT NULL FOR DEBUG ONLY; CHANGE TO NOT NULL IN RELEASE
+                            "album_id INTEGER NOT NULL" //Album in which the card need to be saved
+                            "FOREIGN KEY(album_id) REFERENCES Album(id));";
     //Error message for SQLite operations
     char* errormessage;
 
@@ -83,6 +91,17 @@ int tables(sqlite3* db) {
     else {
         //Debug check
         std::cout << "Cards created/found successfully" << std::endl;
+    }
+
+    dbo = sqlite3_exec(db, CData, nullptr, nullptr, &errormessage);
+    if(dbo != SQLITE_OK) {
+        //In case of errors, throws an error code and abort the function. Main app will also close to prevent data loss and errors.
+        std::cerr << "SQL error: " << errormessage << std::endl;
+        return dbo;
+    }
+    else {
+        //Debug check
+        std::cout << "Card data created/found successfully" << std::endl;
     }
 
     return dbo;
@@ -209,4 +228,54 @@ int make_cell(sqlite3* db, int album_id) {
     sqlite3_finalize(stmt);
 
     return dbo;
+}
+
+int make_card(sqlite3* db, std::string name, int NCards) {
+    //Check if the card name has an entry in the data table
+    const char* sql1 = "SELECT * FROM CData WHERE name = ?;";
+
+    sqlite3_stmt* stmt;
+
+    int dbo = sqlite3_prepare_v2(db, sql1, -1, &stmt, nullptr);
+    if(dbo != SQLITE_OK) {
+        //Error message thrown, function stops
+        std::cerr << "Statement preparation error" << sqlite3_errmsg(db) << std::endl;
+        return dbo;
+    }
+    else {
+        //Debug check
+        std::cout << "Search Statement created correctly" << std::endl;
+    }
+
+    sqlite3_bind_text(stmt, 1, name.c_str(), -1, nullptr);
+
+    if(sqlite3_step(stmt) == SQLITE_ROW) {
+        std::cout << "Found successfully" << std::endl;
+        // 1: Save the album data in variable
+        // 2: Record in variable that the card exists (and will likely already have a cell entry)
+    }
+    else {
+        //Debug check
+        std::cout << "Card " << name << " not found" << std::endl;
+        // 1: Open the "insert new card log" to record all infos
+        // 2: Save album data in variable
+        // 3: Record in variable that the card has never been recorded until now (so searching for a cell is pointless)
+    }
+
+    //Repeat below for as many times as NCards
+    //If the card was existing
+    // 1: Look for the first free cell. Create new if none found or if all found are filled
+    // 2: Record the cell id
+    // 3: Make card with all given infos
+    // 4: Record the card data in the first free slot of the CellCards table matching the ID
+    // 5: check if all 4 slots are filled and, if so, update the cell filled flag in database
+    //If the card was non existing
+    // 1: Create new cell in the right album
+    // 2: Record the cell id
+    // 3: make card with all given infos
+    // 4: record card data in the first card slot of the CellCard matching the ID
+    // 5: update the "existing" to mark the card exist (can be made significantly more efficient by recording automatically up to 4 cards here, up to 4 cards in the next one ecc, will evaluate in future updates)
+
+    sqlite3_finalize(stmt);
+
 }
